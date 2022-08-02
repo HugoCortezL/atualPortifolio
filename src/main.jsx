186d6dabce4d886b5651f4cf4ac2902ed82639ac
unknown <hugo.cortez@agilesolutions.com>
2022-08-02 06:09:39 -0300

    removing tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import GlobalStyle from './styles/global.tsx'
+import GlobalStyle from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
